3c1a39c0f7dc22844c07b576178167b29a56cdfd
albert chingis <albertchingis@alberts-MBP.fios-router.home>
2019-10-21 11:49:38 -0400

    in process

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _=require("lodash");
+const mongoose=require("mongoose");
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+  res.send("hi");
+});
+
+app.listen(process.env.PORT||3002, function() {
+  console.log("Server started on port 3002~");
+});
